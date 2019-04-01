0bfc78d3574502b7d38bf4e4b4957695fbcec29b
Erkinavci <46395501+Erkinavci@users.noreply.github.com>
2019-04-01 12:21:06 +0300

    Komutlar/temizle.js

---
+++ komutlar/temizle.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+  if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`temizle` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) {
+    const botunmesajyonet = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField(':x: Yetkin yetersiz.', 'Mesajları silebilmem için `Mesajları Yönet` yetkisine sahip olmalıyım.')
+    return message.author.sendEmbed(botunmesajyonet);
+  }
+  let messagecount = parseInt(args.join(' '));
+  message.channel.fetchMessages({
+    limit: messagecount
+  }).then(messages => message.channel.bulkDelete(messages));
+    const sohbetsilindi = new Discord.RichEmbed()
+    .setColor(0x36393E)
+  
+    .setDescription('Mesajlar silindi!\n**Silinen mesaj sayısı:** 50\n**Mesaj kanalı:**' + ` ${message.channel}`)
+    .setFooter(`Bu mesaj 10 saniye sonra silinecektir, dikkat et ölme.`, client.user.avatarURL)
+    //return message.channel.sendEmbed(sohbetsilindi).then(m => m.delete(10000));
+    
+    message.channel.send(':white_check_mark: | **`100`** tane mesaj temizlendi.\n\n:white_small_square: | Kanal:' + ` **${message.channel}**` + '\n:white_small_square: | Bu mesaj **`10`** saniye sonra patlayacaktır, dikkat et ölme.').then(m => m.delete(10000));
+ 
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sil', 'temizçek', 't', 'temizle', 'temiz', 'clear', 'prune'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'temizle',
+  description: 'Belirlenen miktar mesajı siler.',
+  usage: 'temizle <temizlenecek mesaj sayısı>'
+};
